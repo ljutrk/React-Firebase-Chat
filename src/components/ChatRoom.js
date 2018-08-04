2f102895a281dd7c891d9e6cf86216dcc4060e9f
ljutrk <ljubomir.trkulja88@gmail.com>
2018-08-04 12:22:10 +0200

    added message window and connected it to state to show hardcoded messages

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -4,7 +4,13 @@ class ChatRoom extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            message: ""
+            message: "",
+            messages: [
+                { id: 0, message: "message 1"},
+                { id: 1, message: "message 2"},
+                { id: 2, message: "message 3"},
+                { id: 3, message: "message 4"}
+            ]
         }
     }
 
@@ -13,11 +19,21 @@ class ChatRoom extends Component {
 
     }
 
+    showMessages = () => {
+        return this.state.messages.map(message => {
+            return <li key={message.id}> {message.message} </li>
+        })
+    }
+
     render() {
-        console.log(this.state.message);
-        
+
         return (
             <div className="ChatRoom">
+                <div className="chat-window">
+                    <ul>
+                        {this.showMessages()}
+                    </ul>
+                </div>
                 <input onChange={this.inputChangeHandler} value={this.state.message} type="text" placeholder="Your Message" />
             </div>
         );
